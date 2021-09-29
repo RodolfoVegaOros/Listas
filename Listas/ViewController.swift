29ae3eda05ec9c1c46dbe440ee61ff43cca13ded
Alumno <ulsa@D110.local>
2021-09-28 17:29:08 -0700

    Listas

--- Listas/ViewController.swift
+++ Listas/ViewController.swift
@@ -8,16 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let nombres = ["Jose", "Ana", "Pedro", "Juan"]
+    let matriculas = ["99999", "99998", "99997", "99996"]
+    let promedios = ["9.1", "8.5", "9.3", "8.7"]
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nombres.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tableView.dequeueReusableCell(withIdentifier: "celdaAlumno")
+        return celda!
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
